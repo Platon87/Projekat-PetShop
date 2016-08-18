17d18fab98c75f519df44670ca0fb82120d06566
Ivan Markovic <platon879257@gmail.com>
2016-08-18 02:36:13 +0200

    ispravke na responsivu

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+/* INDEX PAGE - CAROUSEL START  */
 
 jQuery(document).ready(function ($) {
     $("#testimonial-slider").owlCarousel({
@@ -11,20 +12,54 @@ jQuery(document).ready(function ($) {
         slideSpeed: 1000,
         autoPlay: false
     });
+
+    /* INDEX PAGE - CAROUSEL END  */
+
+    /* INDEX PAGE - DATETIMEPICKER START  */
+
+    $('.form_date').datetimepicker({
+        weekStart: 1,
+        todayBtn: 1,
+        autoclose: 1,
+        todayHighlight: 1,
+        startView: 2,
+        minView: 2,
+        forceParse: 0
+    });
+    $('.form_time').datetimepicker({
+        language: 'fr',
+        weekStart: 1,
+        todayBtn: 1,
+        autoclose: 1,
+        todayHighlight: 1,
+        startView: 1,
+        minView: 0,
+        maxView: 1,
+        forceParse: 0
+    });
+
+    /* INDEX PAGE - DATETIMEPICKER END  */
+
+    /* BLOG SINGLE PAGE -  FANCY BOX START  */
+
+    $("#fancybox1,#fancybox2,#fancybox3,#fancybox4").fancybox({
+        openEffect: 'elastic',
+        closeEffect: 'elastic',
+        helpers: {
+            title: {
+                type: 'inside'
+            }
+        }
+
+    });
+
+    /* BLOG SINGLE PAGE -  FANCY BOX END  */
+
+
+
+
+
+
+
 });
 
-/* FANCY BOX - BLOG SINGLE START  */
-
- $("#fancybox1,#fancybox2,#fancybox3,#fancybox4").fancybox({
-          openEffect	: 'elastic',
-    	closeEffect	: 'elastic',
-
-    	helpers : {
-    		title : {
-    			type : 'inside'
-    		}
-    	}
-
-});
-
-/* FANCY BOX - BLOG SINGLE END  */
